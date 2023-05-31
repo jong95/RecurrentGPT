edcdf9e58605a6ba49e4a52cb2f5ccc45f7acf0a
jong95 <jong95@gmail.com>
2023-05-31 17:39:47 +0900

    Fix step function. Call step function instead of step_with_edit function.

--- gradio_server.py
+++ gradio_server.py
@@ -21,6 +21,7 @@ _CACHE = {}
 # Build the semantic search model
 embedder = SentenceTransformer('multi-qa-mpnet-base-cos-v1')
 
+
 def init_prompt(novel_type, description):
     if description == "":
         description = ""
@@ -49,6 +50,7 @@ Make sure to be precise and follow the output format strictly.
 
 """
 
+
 def init(novel_type, description, request: gr.Request):
     if novel_type == "":
         novel_type = "Science Fiction"
@@ -56,17 +58,22 @@ def init(novel_type, description, request: gr.Request):
     cookie = request.headers['cookie']
     cookie = cookie.split('; _gat_gtag')[0]
     # prepare first init
-    init_paragraphs = get_init(text=init_prompt(novel_type,description))
+    init_paragraphs = get_init(text=init_prompt(novel_type, description))
     # print(init_paragraphs)
     start_input_to_human = {
         'output_paragraph': init_paragraphs['Paragraph 3'],
-        'input_paragraph': '\n\n'.join([init_paragraphs['Paragraph 1'], init_paragraphs['Paragraph 2']]),
+        'input_paragraph': '\n\n'.join(
+            [init_paragraphs['Paragraph 1'],
+             init_paragraphs['Paragraph 2']]),
         'output_memory': init_paragraphs['Summary'],
-        "output_instruction": [init_paragraphs['Instruction 1'], init_paragraphs['Instruction 2'], init_paragraphs['Instruction 3']]
-    }
+        "output_instruction":
+        [init_paragraphs['Instruction 1'],
+         init_paragraphs['Instruction 2'],
+         init_paragraphs['Instruction 3']]}
 
     _CACHE[cookie] = {"start_input_to_human": start_input_to_human,
                       "init_paragraphs": init_paragraphs}
+
     written_paras = f"""Title: {init_paragraphs['name']}
 
 Outline: {init_paragraphs['Outline']}
@@ -74,11 +81,17 @@ Outline: {init_paragraphs['Outline']}
 Paragraphs:
 
 {start_input_to_human['input_paragraph']}"""
-    long_memory = parse_instructions([init_paragraphs['Paragraph 1'], init_paragraphs['Paragraph 2']])
+
+    long_memory = parse_instructions(
+        [init_paragraphs['Paragraph 1'],
+         init_paragraphs['Paragraph 2']])
     # short memory, long memory, current written paragraphs, 3 next instructions
     return start_input_to_human['output_memory'], long_memory, written_paras, init_paragraphs['Instruction 1'], init_paragraphs['Instruction 2'], init_paragraphs['Instruction 3']
 
-def step(short_memory, long_memory, instruction1, instruction2, instruction3, current_paras, request: gr.Request, ):
+
+def step(
+        short_memory, long_memory, instruction1, instruction2, instruction3,
+        current_paras, request: gr.Request,):
     if current_paras == "":
         return "", "", "", "", "", ""
     global _CACHE
@@ -100,8 +113,11 @@ def step(short_memory, long_memory, instruction1, instruction2, instruction3, cu
         writer_start_input = human.output
 
         # Init writerGPT
-        writer = RecurrentGPT(input=writer_start_input, short_memory=start_short_memory, long_memory=[
-            init_paragraphs['Paragraph 1'], init_paragraphs['Paragraph 2']], memory_index=None, embedder=embedder)
+        writer = RecurrentGPT(
+            input=writer_start_input, short_memory=start_short_memory,
+            long_memory=[init_paragraphs['Paragraph 1'],
+                         init_paragraphs['Paragraph 2']],
+            memory_index=None, embedder=embedder)
         cache["writer"] = writer
         cache["human"] = human
         writer.step()
@@ -122,7 +138,9 @@ def step(short_memory, long_memory, instruction1, instruction2, instruction3, cu
     return writer.output['output_memory'], long_memory, current_paras + '\n\n' + writer.output['input_paragraph'], human.output['output_instruction'], *writer.output['output_instruction']
 
 
-def controled_step(short_memory, long_memory, selected_instruction, current_paras, request: gr.Request, ):
+def controled_step(
+        short_memory, long_memory, selected_instruction, current_paras,
+        request: gr.Request,):
     if current_paras == "":
         return "", "", "", "", "", ""
     global _CACHE
@@ -137,13 +155,18 @@ def controled_step(short_memory, long_memory, selected_instruction, current_para
         init_paragraphs = cache["init_paragraphs"]
         human = Human(input=start_input_to_human,
                       memory=None, embedder=embedder)
-        human.step_with_edit()
+        # * Patch.
+        # human.step_with_edit()
+        human.step()
         start_short_memory = init_paragraphs['Summary']
         writer_start_input = human.output
 
         # Init writerGPT
-        writer = RecurrentGPT(input=writer_start_input, short_memory=start_short_memory, long_memory=[
-            init_paragraphs['Paragraph 1'], init_paragraphs['Paragraph 2']], memory_index=None, embedder=embedder)
+        writer = RecurrentGPT(
+            input=writer_start_input, short_memory=start_short_memory,
+            long_memory=[init_paragraphs['Paragraph 1'],
+                         init_paragraphs['Paragraph 2']],
+            memory_index=None, embedder=embedder)
         cache["writer"] = writer
         cache["human"] = human
         writer.step()
@@ -154,12 +177,16 @@ def controled_step(short_memory, long_memory, selected_instruction, current_para
         output['output_memory'] = short_memory
         output['output_instruction'] = selected_instruction
         human.input = output
-        human.step_with_edit()
+        # * Patch.
+        # human.step_with_edit()
+        human.step()
         writer.input = human.output
         writer.step()
 
     # short memory, long memory, current written paragraphs, 3 next instructions
-    return writer.output['output_memory'], parse_instructions(writer.long_memory), current_paras + '\n\n' + writer.output['input_paragraph'], *writer.output['output_instruction']
+    return writer.output['output_memory'], parse_instructions(
+        writer.long_memory), current_paras + '\n\n' + writer.output[
+        'input_paragraph'], *writer.output['output_instruction']
 
 
 # SelectData is a subclass of EventData
@@ -182,22 +209,29 @@ with gr.Blocks(title="RecurrentGPT", css="footer {visibility: hidden}", theme="d
                     with gr.Row():
                         with gr.Column(scale=1, min_width=200):
                             novel_type = gr.Textbox(
-                                label="Novel Type", placeholder="e.g. science fiction")
+                                label="Novel Type",
+                                placeholder="e.g. science fiction")
                         with gr.Column(scale=2, min_width=400):
                             description = gr.Textbox(label="Description")
                 btn_init = gr.Button(
                     "Init Novel Generation", variant="primary")
-                gr.Examples(["Science Fiction", "Romance", "Mystery", "Fantasy",
-                            "Historical", "Horror", "Thriller", "Western", "Young Adult", ], inputs=[novel_type])
+                gr.Examples(
+                    ["Science Fiction", "Romance", "Mystery", "Fantasy",
+                     "Historical", "Horror", "Thriller", "Western",
+                     "Young Adult", ],
+                    inputs=[novel_type])
                 written_paras = gr.Textbox(
-                    label="Written Paragraphs (editable)", max_lines=21, lines=21)
+                    label="Written Paragraphs (editable)",
+                    max_lines=21, lines=21)
             with gr.Column():
                 with gr.Box():
                     gr.Markdown("### Memory Module\n")
                     short_memory = gr.Textbox(
-                        label="Short-Term Memory (editable)", max_lines=3, lines=3)
+                        label="Short-Term Memory (editable)",
+                        max_lines=3, lines=3)
                     long_memory = gr.Textbox(
-                        label="Long-Term Memory (editable)", max_lines=6, lines=6)
+                        label="Long-Term Memory (editable)",
+                        max_lines=6, lines=6)
                     # long_memory = gr.Dataframe(
                     #     # label="Long-Term Memory (editable)",
                     #     headers=["Long-Term Memory (editable)"],
@@ -211,20 +245,30 @@ with gr.Blocks(title="RecurrentGPT", css="footer {visibility: hidden}", theme="d
                     gr.Markdown("### Instruction Module\n")
                     with gr.Row():
                         instruction1 = gr.Textbox(
-                            label="Instruction 1 (editable)", max_lines=4, lines=4)
+                            label="Instruction 1 (editable)",
+                            max_lines=4, lines=4)
                         instruction2 = gr.Textbox(
-                            label="Instruction 2 (editable)", max_lines=4, lines=4)
+                            label="Instruction 2 (editable)",
+                            max_lines=4, lines=4)
                         instruction3 = gr.Textbox(
-                            label="Instruction 3 (editable)", max_lines=4, lines=4)
+                            label="Instruction 3 (editable)",
+                            max_lines=4, lines=4)
                     selected_plan = gr.Textbox(
-                        label="Revised Instruction (from last step)", max_lines=2, lines=2)
+                        label="Revised Instruction (from last step)",
+                        max_lines=2, lines=2)
 
                 btn_step = gr.Button("Next Step", variant="primary")
 
-        btn_init.click(init, inputs=[novel_type, description], outputs=[
-            short_memory, long_memory, written_paras, instruction1, instruction2, instruction3])
-        btn_step.click(step, inputs=[short_memory, long_memory, instruction1, instruction2, instruction3, written_paras], outputs=[
-            short_memory, long_memory, written_paras, selected_plan, instruction1, instruction2, instruction3])
+        btn_init.click(
+            init, inputs=[novel_type, description],
+            outputs=[short_memory, long_memory, written_paras, instruction1,
+                     instruction2, instruction3])
+        btn_step.click(
+            step,
+            inputs=[short_memory, long_memory, instruction1, instruction2,
+                    instruction3, written_paras],
+            outputs=[short_memory, long_memory, written_paras, selected_plan,
+                     instruction1, instruction2, instruction3])
 
     with gr.Tab("Human-in-the-Loop"):
         with gr.Row():
@@ -233,47 +277,68 @@ with gr.Blocks(title="RecurrentGPT", css="footer {visibility: hidden}", theme="d
                     with gr.Row():
                         with gr.Column(scale=1, min_width=200):
                             novel_type = gr.Textbox(
-                                label="Novel Type", placeholder="e.g. science fiction")
+                                label="Novel Type",
+                                placeholder="e.g. science fiction")
                         with gr.Column(scale=2, min_width=400):
                             description = gr.Textbox(label="Description")
                 btn_init = gr.Button(
                     "Init Novel Generation", variant="primary")
-                gr.Examples(["Science Fiction", "Romance", "Mystery", "Fantasy",
-                            "Historical", "Horror", "Thriller", "Western", "Young Adult", ], inputs=[novel_type])
+                gr.Examples(
+                    ["Science Fiction", "Romance", "Mystery", "Fantasy",
+                     "Historical", "Horror", "Thriller", "Western",
+                     "Young Adult", ],
+                    inputs=[novel_type])
                 written_paras = gr.Textbox(
-                    label="Written Paragraphs (editable)", max_lines=23, lines=23)
+                    label="Written Paragraphs (editable)",
+                    max_lines=23, lines=23)
             with gr.Column():
                 with gr.Box():
                     gr.Markdown("### Memory Module\n")
                     short_memory = gr.Textbox(
-                        label="Short-Term Memory (editable)", max_lines=3, lines=3)
+                        label="Short-Term Memory (editable)",
+                        max_lines=3, lines=3)
                     long_memory = gr.Textbox(
-                        label="Long-Term Memory (editable)", max_lines=6, lines=6)
+                        label="Long-Term Memory (editable)",
+                        max_lines=6, lines=6)
                 with gr.Box():
                     gr.Markdown("### Instruction Module\n")
                     with gr.Row():
                         instruction1 = gr.Textbox(
-                            label="Instruction 1", max_lines=3, lines=3, interactive=False)
+                            label="Instruction 1", max_lines=3, lines=3,
+                            interactive=False)
                         instruction2 = gr.Textbox(
-                            label="Instruction 2", max_lines=3, lines=3, interactive=False)
+                            label="Instruction 2", max_lines=3, lines=3,
+                            interactive=False)
                         instruction3 = gr.Textbox(
-                            label="Instruction 3", max_lines=3, lines=3, interactive=False)
+                            label="Instruction 3", max_lines=3, lines=3,
+                            interactive=False)
                     with gr.Row():
                         with gr.Column(scale=1, min_width=100):
-                            selected_plan = gr.Radio(["Instruction 1", "Instruction 2", "Instruction 3"], label="Instruction Selection",)
-                                                    #  info="Select the instruction you want to revise and use for the next step generation.")
+                            selected_plan = gr.Radio(
+                                ["Instruction 1", "Instruction 2",
+                                 "Instruction 3"],
+                                label="Instruction Selection",)
+                            #  info="Select the instruction you want to revise and use for the next step generation.")
                         with gr.Column(scale=3, min_width=300):
                             selected_instruction = gr.Textbox(
-                                label="Selected Instruction (editable)", max_lines=5, lines=5)
+                                label="Selected Instruction (editable)",
+                                max_lines=5, lines=5)
 
                 btn_step = gr.Button("Next Step", variant="primary")
 
-        btn_init.click(init, inputs=[novel_type, description], outputs=[
-            short_memory, long_memory, written_paras, instruction1, instruction2, instruction3])
-        btn_step.click(controled_step, inputs=[short_memory, long_memory, selected_instruction, written_paras], outputs=[
-            short_memory, long_memory, written_paras, instruction1, instruction2, instruction3])
-        selected_plan.select(on_select, inputs=[
-                             instruction1, instruction2, instruction3], outputs=[selected_instruction])
+        btn_init.click(
+            init, inputs=[novel_type, description],
+            outputs=[short_memory, long_memory, written_paras, instruction1,
+                     instruction2, instruction3])
+        btn_step.click(
+            controled_step,
+            inputs=[short_memory, long_memory, selected_instruction,
+                    written_paras],
+            outputs=[short_memory, long_memory, written_paras, instruction1,
+                     instruction2, instruction3])
+        selected_plan.select(
+            on_select, inputs=[instruction1, instruction2, instruction3],
+            outputs=[selected_instruction])
 
     demo.queue(concurrency_count=1)
 
